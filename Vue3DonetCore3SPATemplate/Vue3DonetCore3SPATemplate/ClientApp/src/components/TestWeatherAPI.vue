c8df5f67f7eb5ed658f071c1c6f7e4df3930ea69
johnny1991524 <johnny1991524@gmail.com>
2020-11-04 13:51:17 +0800

    init

---
+++ Vue3DonetCore3SPATemplate/Vue3DonetCore3SPATemplate/ClientApp/src/components/TestWeatherAPI.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+    {{weather}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'TestWeatherAPI',
+  setup(){
+    const weather: any = ref([123]);
+
+    onMounted(()=>{
+      axios.get(process.env.VUE_APP_SERVER_URL + 'WeatherForecast').then(response =>{
+        weather.value = response.data
+        console.log(weather)
+      });
+    })
+
+    return {
+      weather
+    }
+  }
+
+});
+</script>
+
